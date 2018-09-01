b0730bdd1e4b6b3a921717b86daa4ba102eebbe2
SupervisedLearner <triast@aol.com>
2018-09-01 00:51:38 -0400

    Added GPU for xgboost and created an R project.

--- digits.R
+++ digits.R
@@ -1,18 +1,16 @@
 library(xgboost)
-library(caret)
-library(e1071)
 library(tidyverse)
 
-filepath <- "data//"
+filepath <- "data"
 filename <- "train.csv"
-fullfilepath <- paste0(filepath, filename)
+fullfilepath <- file.path(filepath, filename)
 traindata_raw <- read.csv(fullfilepath)
 
 filename <- "test.csv"
-fullfilepath <- paste0(filepath, filename)
+fullfilepath <- file.path(filepath, filename)
 testdata_raw <- read.csv(fullfilepath)
 
-filepath <- ""
+filepath <- "/"
 output_filename <-"output.csv"
 fullfilepath <- paste0(filepath, output_filename)
 
@@ -27,10 +25,11 @@ testdata<-mapply(as.numeric, testdata_raw)
 elapsed_time<-system.time(cvresults <- xgb.cv(
   params    = list(objective="multi:softmax",
                    num_class=10,
-                   eta=0.3,
-                   max_depth=6),
+                   eta=1e-3,
+                   max_depth=8,
+                   tree_method=gpu_exact), #requires GPU version of xgboost
   data      = traindata, 
-  nrounds   = 500,
+  nrounds   = 2000,
   nfold     = 5,
   label     = trainoutput,
   early_stopping_rounds = 10,
@@ -40,15 +39,16 @@ elapsed_time
 
 #Initial Training using xgboost
 elapsed_time<-system.time(model <- xgboost(
-  data      = traindata, 
-  label     = trainoutput,
-  max_depth = 6, 
-  eta       = 0.3, 
-  #nthread   = 4, 
-  nrounds   = 124, # based on CV 
-  num_class = 10,
-  objective = "multi:softmax", 
-  verbose   = 1))
+  data        = traindata, 
+  label       = trainoutput,
+  tree_method = gpu_exact,
+  max_depth   = 8, 
+  eta         = 1e-3, 
+  #nthread     = 4, 
+  nrounds     = cvresults$best_iteration, # based on CV 
+  num_class   = 10,
+  objective   = "multi:softmax", 
+  verbose     = 1))
 
 elapsed_time
 
